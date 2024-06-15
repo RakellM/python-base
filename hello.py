cc3ec40842d5b023522dd3125c3df58f1198050c
Rakell <raquelsmarques@gmail.com>
2024-06-15 18:27:38 -0400

    Implementamos o hello world multi linguas.

--- hello.py
+++ hello.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
+"""Hello World Multi Languages.
 
-# Este programa imprime Hello World
-print("Hello, World!")
+Depending on the configured language in the environment the code will shown 
+the mesaage accordingly.
+
+How to:
+
+Have a LANG variable configured eg.
+
+    export LANG=pt_BR
+
+Execution:
+
+    python3 hello.py
+    or
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Raquel Marques"
+__license__ = "Unlicense"
+
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print(msg)
