8f02e961703f1423f24bffdff94b21da60e8b89d
Rakell <raquelsmarques@gmail.com>
2024-07-01 22:33:36 -0400

    Exemplo com dicionário complexo.

--- cadastro_produto.py
+++ cadastro_produto.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python3
 """Cadastro de produto"""
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Raquel Marques"
 
 produto = {
     "nome": "Caneta",
-    "cor1": "azul",
-    "cor2": "branco",
+    "cores": ["azul", "branco"],
     "preco": 3.23 ,
-    "dimensao_altura": 12.1 ,
-    "dimensao_largura": 0.8 ,
+    "dimensao": {
+        "altura": 12.1 ,
+        "largura": 0.8 
+    },
     "em_estoque": True ,
     "codigo": 45678 ,
     "codebar": None
 }
 
-compra = ("João", produto["nome"], 3)
-total_compra = compra[2] * produto["preco"]
+cliente = {
+    "nome": "João"
+}
+
+compra = {
+    "cliente": cliente,
+    "produto": produto,
+    "quantidade": 3
+}
+
+total_compra = compra["quantidade"] * compra["produto"]["preco"]
 
 print(
-    f"O cliente {compra[0]}"
-    f" comprou {compra[1]}"
+    f"O cliente {compra["cliente"]["nome"]}"
+    f" comprou {compra["quantidade"]} unidates de {compra["produto"]["nome"]}"
     f" e pagou o total de {total_compra}"
 )
