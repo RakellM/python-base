b8227b17d16a52192ce4c8709875be40753e420b
Rakell <raquelsmarques@gmail.com>
2024-07-01 22:22:56 -0400

    Exemplo com dicionário simples.

--- cadastro_produto.py
+++ cadastro_produto.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 """Cadastro de produto"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Raquel Marques"
 
-produto_nome = "Caneta"
-produto_cor1 = "azul"
-produto_cor2 = "branco"
-produto_preco = 3.23
-produto_dimensao_altura = 12.1
-produto_dimensao_largura = 0.8
-produto_em_estoque = True
-produto_codigo = 45678
-produto_codebar = None
+produto = {
+    "nome": "Caneta",
+    "cor1": "azul",
+    "cor2": "branco",
+    "preco": 3.23 ,
+    "dimensao_altura": 12.1 ,
+    "dimensao_largura": 0.8 ,
+    "em_estoque": True ,
+    "codigo": 45678 ,
+    "codebar": None
+}
 
-compra = ("João", produto_nome, 3)
-total_compra = compra[2] * produto_preco
+compra = ("João", produto["nome"], 3)
+total_compra = compra[2] * produto["preco"]
 
 print(
     f"O cliente {compra[0]}"
