ff73f6f65ed135648a34049a3b41755e5ea8083e
Rakell <raquelsmarques@gmail.com>
2024-08-26 12:58:54 -0400

    Usando Input e argumentos do CLI

--- hello.py
+++ hello.py
@@ -16,20 +16,47 @@ Execution:
     or
     ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Raquel Marques"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+print(f"{sys.argv}")
+arguments = {
+    "lang": None,
+    "count": 1
+}
+for arg in sys.argv[1:]:
+#    print(f"{arg=}")
+#    print(arg.split("="))
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+#    print(key, value)
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+
+current_language = current_language[:5]
 
 msg = {
-    "en-US": "Hello, World!",
+    "en_US": "Hello, World!",
     "pt_BR": "Olá, Mundo!",
     "it_IT": "Ciao, Mondo!",
     "es_SP": "Hola, Mundo!",
     "fr_FR": "Bonjour, Monde!",
 }
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
