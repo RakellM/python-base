e716d9e2622bc27494ebe16d6e8867eff7513458
Rakell <raquelsmarques@gmail.com>
2024-09-07 18:44:18 -0400

    Adicionado o arquivo log para gravação das operações efetuados, com um timestamp.

--- prefixcal.py
+++ prefixcal.py
@@ -24,11 +24,13 @@ n1: 5
 n2: 4
 9
 """
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Raquel Marques"
 __license__ = "Unlicense"
 
+import os
 import sys
+from datetime import datetime
 
 arguments = sys.argv[1:]
 
@@ -74,5 +76,15 @@ elif operation == "mul":
     result = n1 * n2
 elif operation == "div":
     result = n1 / n2
+
+path = os.curdir
+filepath = os.path.join(path, "prefixcal.log")
+timestamp = datetime.now().isoformat()
+user = os.getenv('USER', 'anonymous')
+
+with open(filepath, "a") as file_:
+    file_.write(f"{timestamp} - {user} - {operation},{n1},{n2} = {result}\n")
+
+#print(f"{operation},{n1},{n2} = {result}", file=open(filepath, "a"))
 
 print(f"O resultado é {result}")
