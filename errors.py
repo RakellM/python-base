de702fe075d14314bbf31405b435c3d7694ff590
Rakell <raquelsmarques@gmail.com>
2024-12-14 18:07:51 -0500

    Error: EAFP + treated exceptions

--- errors.py
+++ errors.py
@@ -6,9 +6,19 @@ import sys
 # EAFP - Easy to Ask Forgiveness than Permission
 
 try:
-    names = open("names.txt").readlines()
-except: # Bare except
+    names = open("names.txt").readlines() # FileNotFoundError
+    1 / 1 # ZeroDivisionError
+    print(names.append) # AttributeError
+except FileNotFoundError:
     print("[Error] File names.txt not found!")
+    sys.exit(1)
+except ZeroDivisionError:
+    print("[Error] You cannnot divide by zero!")
+    sys.exit(1)
+except AttributeError:
+    print("[Error] List does not have banana.")
+    sys.exit(1)
+
 
 try:
     print(names[2])
