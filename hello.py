652c6584b1c14304c4f21be661c6fb25eb100b86
Rakell <raquelsmarques@gmail.com>
2024-12-14 18:48:32 -0500

    Fixed TODO ValueError, used try/except to identify wrong language input

--- hello.py
+++ hello.py
@@ -29,10 +29,16 @@ arguments = {
     "count": 1
 }
 for arg in sys.argv[1:]:
-#    print(f"{arg=}")
-#    print(arg.split("="))
     # TODO: Tratar ValueError
-    key, value = arg.split("=")
+    try:
+        key, value = arg.split("=")
+    except ValueError as e:
+        # TODO: Logging
+        print(f"[ERROR] {str(e)}")
+        print("You need to use `=`")
+        print(f"You passed {arg}")
+        print("try with --key=value")
+        sys.exit(1)
     key = key.lstrip("-").strip()
     value = value.strip()
     if key not in arguments:
@@ -59,4 +65,17 @@ msg = {
     "fr_FR": "Bonjour, Monde!",
 }
 
-print(msg[current_language] * int(arguments["count"]))
+"""
+# Try with default value
+message = msg.get(current_language, msg["en_US"])
+"""
+
+#EAFP
+try:
+    message = msg[current_language]
+except KeyError as e:
+    print(f"[ERROR] {str(e)}")
+    print(f"Language is invalid, choose from: {list(msg.keys())}")
+    sys.exit(1)
+
+print(message * int(arguments["count"]))
