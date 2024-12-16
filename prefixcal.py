aad605f6bdf3609c26c84f60982a6aefeb8844c8
Rakell <raquelsmarques@gmail.com>
2024-12-15 21:17:17 -0500

    Used WHILE and ask the user to continue.

--- prefixcal.py
+++ prefixcal.py
@@ -24,7 +24,7 @@ n1: 5
 n2: 4
 9
 """
-__version__ = "0.2.1"
+__version__ = "0.3.1"
 __author__ = "Raquel Marques"
 __license__ = "Unlicense"
 
@@ -32,70 +32,76 @@ import os
 import sys
 from datetime import datetime
 
-arguments = sys.argv[1:]
+while True:
 
-# Validation
-if not arguments:
-    operation = input("operation:")
-    n1 = input("n1:")
-    n2 = input("n2:")
-    arguments = [operation, n1, n2]
-elif len(arguments) != 3:
-    print("Invalid number of arguments")
-    print("ex: `sum 5 5")
-    sys.exit(1)
+    arguments = sys.argv[1:]
 
-operation, *nums = arguments
-
-valid_operations = ("sum", "sub", "mul", "div")
-if operation not in valid_operations:
-    print("Invalid operation!")
-    print(valid_operations)
-    sys.exit(1)
-
-validated_nums = []
-for num in nums:
-    # TODO: Repetiiton WHILE + exceptions
-    if not num.replace(".", "").isdigit():
-        print(f"Invalid number {num}")
+    # Validation
+    if not arguments:
+        operation = input("operation:")
+        n1 = input("n1:")
+        n2 = input("n2:")
+        arguments = [operation, n1, n2]
+    elif len(arguments) != 3:
+        print("Invalid number of arguments")
+        print("ex: `sum 5 5")
         sys.exit(1)
-    if "." in num:
-        num = float(num)
-    else:
-        num = int(num)
-    validated_nums.append(num)
 
-try:
-    n1, n2 = validated_nums
-except ValueError as e:
-    print(f"{str(e)}")
-    sys.exit(1)
+    operation, *nums = arguments
 
-# TODO: Use functions dictionary
-if operation == "sum":
-    result = n1 + n2
-elif operation == "sub":
-    result = n1 - n2
-elif operation == "mul":
-    result = n1 * n2
-elif operation == "div":
-    result = n1 / n2
+    valid_operations = ("sum", "sub", "mul", "div")
+    if operation not in valid_operations:
+        print("Invalid operation!")
+        print(valid_operations)
+        sys.exit(1)
 
-path = os.curdir
-filepath = os.path.join(path, "prefixcal.log")
-timestamp = datetime.now().isoformat()
-user = os.getenv('USER', 'anonymous')
+    validated_nums = []
+    for num in nums:
+        # TODO: Repetition WHILE + exceptions
+        if not num.replace(".", "").isdigit():
+            print(f"Invalid number {num}")
+            sys.exit(1)
+        if "." in num:
+            num = float(num)
+        else:
+            num = int(num)
+        validated_nums.append(num)
 
-#print(f"{operation},{n1},{n2} = {result}", file=open(filepath, "a"))
+    try:
+        n1, n2 = validated_nums
+    except ValueError as e:
+        print(f"{str(e)}")
+        sys.exit(1)
 
-print(f"The result is {result}")
+    # TODO: Use functions dictionary
+    if operation == "sum":
+        result = n1 + n2
+    elif operation == "sub":
+        result = n1 - n2
+    elif operation == "mul":
+        result = n1 * n2
+    elif operation == "div":
+        result = n1 / n2
 
-try:
-    with open(filepath, "a") as file_:
-        file_.write(f"{timestamp} - {user} - {operation},{n1},{n2} = {result}\n")
-except PermissionError as e:
-    # TODO: logging
-    print(f"{str(e)}")
-    sys.exit(1)
+    path = os.curdir
+    filepath = os.path.join(path, "prefixcal.log")
+    timestamp = datetime.now().isoformat()
+    user = os.getenv('USER', 'anonymous')
+
+    #print(f"{operation},{n1},{n2} = {result}", file=open(filepath, "a"))
+
+    print(f"The result is {result}")
+
+    try:
+        with open(filepath, "a") as file_:
+            file_.write(f"{timestamp} - {user} - {operation},{n1},{n2} = {result}\n")
+    except PermissionError as e:
+        # TODO: logging
+        print(f"{str(e)}")
+        sys.exit(1)
+    
+    if input("Press ENTER to continue or any other key to leave."):
+        break
+
 
 
