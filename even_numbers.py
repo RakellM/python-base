123e3ca425dbd1ee23af7644e1fdc6efb69748b3
Rakell <raquelsmarques@gmail.com>
2025-02-08 10:49:21 -0500

    docs: Create a script that show on the screen all the even numbers from 1 to 200.

---
+++ even_numbers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Create a script that show on the screen all the even numbers from 1 to 200.
+
+example:
+`python3 even_numbers.py`
+2
+4
+6
+8
+...
+
+"""
+
+__version__ = "0.1.0"
+__author__ = "Raquel Marques"
+__license__ = "Unlicense"
+
+array_nbrs = range (1, 201)
+
+for nbr in array_nbrs:
+    even = nbr % 2 == 0
+    if even:
+        print(nbr)
+    else:
+        continue
