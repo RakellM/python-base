ce501a6fadb36909d0552f730fd2047fdc24266b
Rakell <raquelsmarques@gmail.com>
2025-02-08 20:08:35 -0500

    docs: fixed bug of while break.

--- temperature_alert.py
+++ temperature_alert.py
@@ -13,7 +13,7 @@ temp less 0: ALERT: Extreme cold
 
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Raquel Marques"
 __license__ = "Unlicense"
 
@@ -22,26 +22,37 @@ import logging
 
 log = logging.Logger("temperature_alert")
 
+# TODO: Use functions to read inputs
+
 info = { 
     "temperature": None ,
     "humidity": None
 } 
 
-keys = info.keys()
+while True:
+    # stop condition: dictionary is fully filled
+    info_size = len(info.values())
+    filled_size = len([value for value in info.values() if value is not None])
+    if info_size == filled_size:
+        break #stop while
 
-for key in keys: 
-    try:
-        info[key] = float(input(f"What is the {key}?").strip())
-    except ValueError:
-        log.error(f"Invalid {key}")
-        sys.exit(1)
+    keys = info.keys()
+
+    for key in keys: 
+        if info[key] is not None:
+            continue
+        try:
+            info[key] = float(input(f"What is the {key}?").strip())
+        except ValueError:
+            log.error(f"Invalid {key}")
+            sys.exit(1)
 
 temp = info['temperature']
 hum = info["humidity"]
 
 if temp > 45:
     print('ALERT!!! 🥵 Danger of extreme heat')
-elif temp * 3 >= hum and temp != 0:
+elif temp >= 30 and temp * 3 >= hum:
     print('ALERT!! 🥵♒ Danger of humid heat')
 elif temp >= 10 and temp < 30:
     print('🙂 Normal')
@@ -49,3 +60,5 @@ elif temp >= 0 and temp < 10:
     print('🥶 Cold')
 elif temp < 0:
     print('ALERT: ⛄ Extreme cold')
+
+# TODO: humidity condition to be between 0 and 100
