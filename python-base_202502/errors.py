cbb2e2eadce593c462e82daec63f71a5c89c100d
Raquel Marques <raquelsmarques@gmail.com>
2025-12-14 20:05:37 -0500

    feat: updating errors. Retry using for pluse recurssive

--- python-base_202502/errors.py
+++ python-base_202502/errors.py
@@ -11,12 +11,18 @@ log = logging.Logger("errors")
 
 def try_to_open_a_file(filepath, retry=1) -> list:
     """Tries to open a file, if error, retries n times."""
+    if retry > 999:
+        raise ValueError("Retry cannot be above 999")
+    
     for attempt in range(1, retry + 1):
         try:
             return open(filepath).readlines() # FileNotFoundError
         except FileNotFoundError as e:
             print("{str(e)}")
             time.sleep(2)
+            if retry > 1:
+                # recurssive
+                return try_to_open_a_file(filepath, retry=retry - 1)
         else:
             print("Success!")
         finally:
